2a37a470a614ef50b7f988bea467b5144796d967
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-12-31 09:40:45 +0100

    #19: Fixed the github subdomain

--- bin/src/extension/github_extensions.dart
+++ bin/src/extension/github_extensions.dart
@@ -3,16 +3,30 @@ import '../model/dto/dependency.dart';
 extension GitInfoExtensions on GitInfo {
   bool isGithubUrl() =>
       url.startsWith('https://github.com/') ||
+      url.startsWith('https://www.github.com/') ||
       url.startsWith('http://github.com/') ||
+      url.startsWith('http://www.github.com/') ||
       url.startsWith('git://github.com/') ||
       url.startsWith('git@github.com:') ||
       url.startsWith('https://raw.githubusercontent.com/') ||
       url.startsWith('http://raw.githubusercontent.com/');
 
   String getGithubPubSpecUrl() {
-    const gitSuffix = '.git';
+    const wwwHttpsPrefix = 'https://www.';
+    const httpPrefix = 'http://';
+    const wwwHttpPrefix = 'http://www.';
     const gitPrefix = 'git@github.com:';
+    const gitSuffix = '.git';
     var newUrl = url;
+    if (newUrl.startsWith(wwwHttpsPrefix)) {
+      newUrl = newUrl.replaceFirst(wwwHttpsPrefix, 'https://');
+    }
+    if (newUrl.startsWith(wwwHttpPrefix)) {
+      newUrl = newUrl.replaceFirst(wwwHttpPrefix, 'https://');
+    }
+    if (newUrl.startsWith(httpPrefix)) {
+      newUrl = newUrl.replaceFirst(httpPrefix, 'https://');
+    }
     if (newUrl.startsWith(gitPrefix)) {
       newUrl = newUrl.replaceFirst(gitPrefix, 'https://github.com/');
     }
--- test/extension/github_extensions_test.dart
+++ test/extension/github_extensions_test.dart
@@ -7,19 +7,39 @@ void main() {
     group('normal', () {
       test('Test getGithubPubSpecUrl with normal git repo', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo.git',
+          url: 'git@github.com:vanlooverenkoen/test-repo.git',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
       });
       test('Test getGithubPubSpecUrl without .git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo',
+          url: 'git@github.com:vanlooverenkoen/test-repo',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
       });
       test('Test getGithubPubSpecUrl with https', () {
         final gitInfo = GitInfo(
           url: 'https://github.com/vanlooverenkoen/test-repo',
+        );
+        expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
+      });
+      test('Test getGithubPubSpecUrl with http', () {
+        final gitInfo = GitInfo(
+          url: 'http://github.com/vanlooverenkoen/test-repo',
+        );
+        expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
+      });
+    });
+    group('www', () {
+      test('Test getGithubPubSpecUrl with https', () {
+        final gitInfo = GitInfo(
+          url: 'https://www.github.com/vanlooverenkoen/test-repo',
+        );
+        expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
+      });
+      test('Test getGithubPubSpecUrl with http', () {
+        final gitInfo = GitInfo(
+          url: 'http://www.github.com/vanlooverenkoen/test-repo',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/pubspec.yaml');
       });
@@ -34,14 +54,14 @@ void main() {
       });
       test('Test getGithubPubSpecUrl with git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo.git',
+          url: 'git@github.com:vanlooverenkoen/test-repo.git',
           ref: '654dsf564dsf4ds5f6',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/654dsf564dsf4ds5f6/pubspec.yaml');
       });
       test('Test getGithubPubSpecUrl without .git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo',
+          url: 'git@github.com:vanlooverenkoen/test-repo',
           ref: '654dsf564dsf4ds5f6',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/654dsf564dsf4ds5f6/pubspec.yaml');
@@ -57,14 +77,14 @@ void main() {
       });
       test('Test getGithubPubSpecUrl with git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo.git',
+          url: 'git@github.com:vanlooverenkoen/test-repo.git',
           path: 'extra-path',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/extra-path/pubspec.yaml');
       });
       test('Test getGithubPubSpecUrl without .git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo',
+          url: 'git@github.com:vanlooverenkoen/test-repo',
           path: 'extra-path',
         );
         expect(gitInfo.getGithubPubSpecUrl(), 'https://raw.githubusercontent.com/vanlooverenkoen/test-repo/master/extra-path/pubspec.yaml');
@@ -81,7 +101,7 @@ void main() {
       });
       test('Test getGithubPubSpecUrl with git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo.git',
+          url: 'git@github.com:vanlooverenkoen/test-repo.git',
           path: 'extra-path',
           ref: '654dsf564dsf4ds5f6',
         );
@@ -89,7 +109,7 @@ void main() {
       });
       test('Test getGithubPubSpecUrl without .git', () {
         final gitInfo = GitInfo(
-          url: 'git://github.com/vanlooverenkoen/test-repo',
+          url: 'git@github.com:vanlooverenkoen/test-repo',
           path: 'extra-path',
           ref: '654dsf564dsf4ds5f6',
         );
