5d45f84eb7ebb9e5c3f8a681640032d1339df1f2
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-01-03 16:45:09 +0100

    Added tests for the webservice

--- bin/src/service/webservice.dart
+++ bin/src/service/webservice.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
+import '../model/exception/fatal_exception.dart';
 import '../util/logger.dart';
 
 @immutable
@@ -12,7 +13,7 @@ class WebService {
     final result = await http.get(Uri.parse(url));
     if (result.statusCode != 200) {
       Logger.logVerbose('Downloading failed: ${result.body}');
-      throw Exception('Failed to get $url');
+      throw FatalException('Failed to get $url');
     }
     Logger.logVerbose('Downloading complete: $url');
     return result.body;
