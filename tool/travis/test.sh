ba631e473a74ae0c5f5f74872afe068c3fbe05bd
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-01-14 16:24:01 +0100

    Added travis script

---
+++ tool/travis/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dart run ./tool/travis/test_coverage_helper.dart || exit -1;
+
+echo ""
+echo "===="
+echo "Start testing"
+echo "===="
+dart run ./tool/test_coverage_helper.dart || exit -1;
+
+rm -rf ./coverage
+
+## Run Dart tests and output them at directory `./coverage`:
+gtimeout 30 dart run test --coverage=./coverage
+
+## Format collected coverage to LCOV (only for directory "lib")
+format_coverage --packages=.packages --report-on=bin --lcov -o ./coverage/lcov.info -i ./coverage
+
+## Generate LCOV report:
+genhtml -o ./coverage/report ./coverage/lcov.info
+
+## Open the HTML coverage report:
+open ./coverage/report/index.html
+
+rm test/coverage_helper_test.dart
+echo "===="
+echo "Finished testing"
+echo "===="
